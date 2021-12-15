bf485d7d94f50aa609c149fae185a6ae682f98a0
JUDONGHYEOK <65863017+JUDONGHYEOK@users.noreply.github.com>
2021-12-15 23:28:39 +0900

    feat: html attributes constant 추가

---
+++ src/constant/index.js
@@ -0,0 +1,9 @@
+export const ID = {
+  START_INPUT: 'departure-station-name-input',
+  END_INPUT: 'arrival-station-name-input',
+  SEARCH_BUTTON: 'search_button'
+};
+
+export const NAME = {
+  RADIO_OPTION: 'search-type'
+};
