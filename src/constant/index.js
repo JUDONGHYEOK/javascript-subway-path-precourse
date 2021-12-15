e914e316b6e1f98d478a294fb48e1c67762efe08
JUDONGHYEOK <65863017+JUDONGHYEOK@users.noreply.github.com>
2021-12-15 23:33:56 +0900

    feat: text constant 정의

--- src/constant/index.js
+++ src/constant/index.js
@@ -7,3 +7,15 @@ export const ID = {
 export const NAME = {
   RADIO_OPTION: 'search-type'
 };
+
+export const TEXT = {
+  TITLE: '지하철 길찾기',
+  START_INPUT_TITLE: '출발역',
+  END_INPUT_TITLE: '도착역',
+  TYPE_DISTANCE: '최단거리',
+  TYPE_TIME: '최소시간',
+  SEARCH_BUTTON: '길 찾기',
+  RESULT_TITLE: '결과',
+  TABLE_TOTAL_DISTANCE: '총 거리',
+  TABLE_TOTAL_TIME: '총 소요 시간'
+};
